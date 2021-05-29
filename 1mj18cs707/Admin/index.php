f3d92e521648c186aed8e7e67aa55262a5b8740b
Sharu2172 <57060263+Sharu2172@users.noreply.github.com>
2021-05-29 14:58:48 +0530

    Update index.php

--- 1mj18cs707/Admin/index.php
+++ 1mj18cs707/Admin/index.php
@@ -1,8 +1,12 @@
-<?php 
-   echo '
-   <form method="POST" id="form_id" action="dashboard/"></form>
-   <script type="text/javascript">
-	   document.forms[\'form_id\'].submit();
-   </script>
-   ';
-?>
+<html>
+	<body>
+		<?php
+		echo '
+   		<form method="POST" id="form_id" action="dashboard/"></form>
+  		<script type="text/javascript">
+	   		document.forms[\'form_id\'].submit();
+   		</script>
+   		';
+		?>
+	</body>
+</html>
